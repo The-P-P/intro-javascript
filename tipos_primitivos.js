c16995bad007c5b9c470ed6dee516e047ae54e26
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2022-12-19 15:28:57 -0300

    commit one

--- tipos_primitivos.js
+++ tipos_primitivos.js
@@ -1,4 +1,31 @@
-let numero1 = 10
-let numero2 = 10
-var soma = numero1 + numero2
-console.log(soma)
+// tipo Number 
+
+const meuNumero =  3; 
+
+const num1 = 1;
+const num2 = 2;
+
+const operacao1 = num1 + num2;
+const operacao2 = num1 * num2;
+
+console.log(operacao1)
+console.log(operacao2)
+
+// ponto flutuante
+
+const num3 =  3.3;
+const num4 = .5;
+
+const operacao3 =  num3 / num4;
+const operacao4 = num3 - num4;
+const operacao5 = num1 / num3;
+
+console.log(operacao3)
+console.log(operacao4)
+console.log(operacao5)
+
+// NaN -> NOT A NUMBER ( nao eh um numero )
+
+const alura  = 'alura'
+
+console.log(alura * num3)
