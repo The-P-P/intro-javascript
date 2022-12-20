07e1db87f1c571635fdd3ef90eaa408182884082
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2022-12-20 12:48:11 -0300

    commit fuor

---
+++ erros.js
@@ -0,0 +1,12 @@
+// log -> registro
+
+const minhavar = true; 
+
+console.log(123)
+console.log('eu sou a lenda')
+console.log(minhavar)
+
+// tratamento de erro!
+
+console.error('nuh ze, deu erro!')
+
