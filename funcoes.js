a3e27d047ced785c9964f6b604ff3efc05015748
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2022-12-20 18:50:42 -0300

    commit 8

---
+++ funcoes.js
@@ -0,0 +1,24 @@
+let x = '';
+console.log(x);
+x = 'oi';
+// o codigo vai ser ledo pelo computador em ordem 
+
+// declaracao de funcao
+
+function imprime(txt){
+    console.log(txt)
+};
+
+// executa a funcao ( 1 ou + vezes)
+
+imprime(soma());
+imprime ('o carro do ovo passando na sua porta ');
+
+// formas de funcoes
+
+function soma(){
+    return 2 + 2;
+
+}
+
+console.log(soma())
