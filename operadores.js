5b4b58604b572f5044653d7a6553fc84771f7d85
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2022-12-20 12:55:11 -0300

    commit five

---
+++ operadores.js
@@ -0,0 +1,22 @@
+// comparacao implicita == (valor) )
+
+const numero = 5;
+const texto = '5'
+
+console.log(numero == texto)
+
+// comparacao implicita === (tipo de dado e o valor) 
+
+console.log(numero === texto)
+
+// typeof 
+
+console.log(typeof numero)
+console.log(typeof texto)
+
+// conversao explicita
+
+Number()
+String()
+
+//
